e6d2175f012eb3537a55efa28b2245fab34328d0
gblajan <george.blajan@8x8.com>
2023-01-21 23:35:49 +0200

    .

--- src/components/utilities/Requests.tsx
+++ src/components/utilities/Requests.tsx
@@ -161,6 +161,7 @@ export async function createDevice(description: string, address: string, maxHour
     }).then(() => {
         state = 0
     }).catch(() => {
+        
         state = 1
     })
 
